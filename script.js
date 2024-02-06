8c59bded1b246957ee1bf183831971632624490c
Abolfazl <125133047+abolfazlzarei@users.noreply.github.com>
2024-02-06 18:08:04 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,34 @@
 function searchGitHubUser(username) {
-  var ajax = new XMLHttpRequest();
-  ajax.open("GET", "https://api.github.com/users/" + username + "/repos", true);
-  ajax.onload = function () {
-    if (ajax.status === 200) {
-      var repositories = JSON.parse(ajax.responseText);
-      displayRepositories(repositories);
-    } else {
-      console.log("Failed to fetch data.");
-    }
-  };
-  ajax.send();
+    var ajax = new XMLHttpRequest();
+    ajax.open("GET", "https://api.github.com/users/" + username + "/repos", true);
+    ajax.onload = function () {
+        if (ajax.status === 200) {
+            var repositories = JSON.parse(ajax.responseText);
+            displayRepositories(repositories);
+        } else {
+            console.log("Failed to fetch data.");
+        }
+    };
+    ajax.send();
 }
 
 function displayRepositories(repositories) {
-  var repositoriesList = document.getElementById("repositories-list");
-  repositoriesList.innerHTML = "";
-  repositories.forEach(function (repo) {
-    var repoItem = document.createElement("div");
-    repoItem.textContent = repo.name;
-    repositoriesList.appendChild(repoItem);
-  });
+    var repositoriesList = document.getElementById("repositories-list");
+    repositoriesList.innerHTML = "";
+    repositories.forEach(function (repo) {
+        var repoItem = document.createElement("div");
+        repoItem.textContent = repo.name;
+        repoItem.style.cursor = "pointer";
+        repoItem.addEventListener("click", function() {
+            window.location.href = repo.html_url;
+        });
+        repositoriesList.appendChild(repoItem);
+    });
 }
 
 var form = document.getElementById("search-form");
 form.addEventListener("submit", function (event) {
-  event.preventDefault();
-  var username = document.getElementById("username").value;
-  searchGitHubUser(username);
+    event.preventDefault();
+    var username = document.getElementById("username").value;
+    searchGitHubUser(username);
 });
